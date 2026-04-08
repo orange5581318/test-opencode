948fb1ecc7f0993e2ce0c6d6c693daac06c791de
邵学文 <2570179569@qq.com>
2026-04-08 14:51:35 +0800

    修改路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import { staticRoutes } from './routes'
 import { getToken } from '@/utils/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory (import.meta.env.BASE_URL),
   routes: staticRoutes,
 })
 
